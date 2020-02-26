e05eb28ab5fed56035dc84fc79bb5d2c0eb2abce
Suhail <suhailm0608@gmail.com>
2020-02-26 20:37:07 +0000

    Object class

--- asynctmdb/__init__.py
+++ asynctmdb/__init__.py
@@ -1,3 +1,4 @@
 from .client import Client
+from .object import Object
 
 __version__ = "0.0.1a"
---
+++ asynctmdb/object.py
@@ -0,0 +1,8 @@
+class Object:
+    def __eq__(self, other):
+        if isinstance(other, Object):
+            try:
+                return type(self) is type(other) and self.id == other.id 
+            except:
+                return super().__eq__(other)
+        return super().__eq__(other)
