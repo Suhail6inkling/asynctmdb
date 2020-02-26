6565788238061d18cb78458311f839209a95f0dc
Suhail <suhailm0608@gmail.com>
2020-02-26 20:51:42 +0000

    Production Company & Country classes

--- asynctmdb/__init__.py
+++ asynctmdb/__init__.py
@@ -2,6 +2,7 @@ from .client import Client
 from .genre import Genre
 from .movie import Movie
 from .object import Object, AttributedDict
+from .production import Company, Country
 from .util import date
 
 __version__ = "0.0.1a"
---
+++ asynctmdb/production.py
@@ -0,0 +1,23 @@
+from .object import Object
+
+class Company(Object):
+
+    def __init__(self, client, data):
+        self.client = client
+        self.name = data.get("name")
+        self.id = data.get("id")
+        self.logo = client.logo(data.get("logo_path"))
+        self.country = data.get("origin_country")
+
+    def __repr__(self):
+        return f"<Company name={self.name!r} id={self.id!r}>"
+
+class Country(Object):
+
+    def __init__(self, client, data):
+        self.client = client
+        self.name = data.get("name")
+        self.code = data.get("iso_639_1")
+
+    def __repr__(self):
+        return f"<Country name={self.name!r}>"
